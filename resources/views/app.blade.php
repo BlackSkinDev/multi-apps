a0bcb539c836ed183fcb6922e55cbc3ae689f444
chillingloccini <chillingloccini1@gmail.com>
2023-05-13 01:37:26 +0100

    Issue FIx

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta charset="utf-8">
+    <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite('resources/css/app.css')
 </head>
